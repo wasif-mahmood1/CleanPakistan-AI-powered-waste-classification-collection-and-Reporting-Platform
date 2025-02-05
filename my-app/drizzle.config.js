f368af3489c78aeff1b329e3b5818cdb20155dc3
wasif-mahmood1 <138897505+wasif-mahmood1@users.noreply.github.com>
2025-02-06 02:04:15 +0500

    Add files via upload

---
+++ my-app/drizzle.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dialect: 'postgresql',
+  schema: "./utils/db/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
+  },
+};
